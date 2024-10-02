e926aa3a48ea83ef26ae625cc1c2a965703e311a
Leander List <llist@student.tgm.ac.at>
2024-10-02 13:45:15 +0200

    Update Controller

--- src/main/java/at/ac/tgm/llist/Controller/WorttrainerController.java
+++ src/main/java/at/ac/tgm/llist/Controller/WorttrainerController.java
@@ -7,17 +7,29 @@ import at.ac.tgm.llist.View.WorttrainerView;
 
 import javax.swing.*;
 
+/**
+ * @author Leander-List
+ * @version 02-10-2024
+ * This class is responsible for the communication between the model and the view
+ */
 public class WorttrainerController {
     private WorttrainerView view;
     private Worttrainer model;
     private Persistence persistence;
 
+    /**
+     * Constructor for the WorttrainerController
+     */
     public WorttrainerController() {
         this.persistence = new JsonPersistence();
         this.model = new Worttrainer();
         this.view = new WorttrainerView(this);
     }
 
+    /**
+     * Constructor for the WorttrainerController
+     * @param persistence the persistence object to use
+     */
     public WorttrainerController(Persistence persistence) {
         if(persistence == null) {
             throw new IllegalArgumentException("Persistence must not be null");
@@ -48,6 +60,10 @@ public class WorttrainerController {
         this.view.setImage(this.model.getPairList().get(this.model.getSelectedPair()).getUrl());
     }
 
+    /**
+     * Saves the model to a file
+     * @param path the path to save the file
+     */
     public void save(String path) {
         try {
             this.persistence.save(this.model, path);
@@ -56,6 +72,10 @@ public class WorttrainerController {
         }
     }
 
+    /**
+     * Checks the input and updates the view
+     * @param input the input to check
+     */
     public void check(String input) {
         if(this.model.checkAnswer(input)) {
             this.view.setCorrectCount(this.model.getCorrectCount());
