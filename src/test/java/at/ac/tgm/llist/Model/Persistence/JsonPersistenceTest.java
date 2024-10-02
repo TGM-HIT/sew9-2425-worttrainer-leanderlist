50264f837728abb1610f8fc4f1e1e17e85d19856
Leander List <llist@student.tgm.ac.at>
2024-10-02 15:00:42 +0200

    Move test.json to resources directory

--- src/test/java/at/ac/tgm/llist/Model/Persistence/JsonPersistenceTest.java
+++ src/test/java/at/ac/tgm/llist/Model/Persistence/JsonPersistenceTest.java
@@ -23,8 +23,8 @@ public class JsonPersistenceTest {
     void testSaveLoad() {
         wt.setCorrectCount(4);
         assertDoesNotThrow(() -> {
-            json.save(wt, "src\\test\\java\\at\\ac\\tgm\\llist\\Model\\Persistence\\test.json");
+            json.save(wt, "src\\test\\resources\\test.json");
         });
-        assertEquals(4, json.load("src\\test\\java\\at\\ac\\tgm\\llist\\Model\\Persistence\\test.json").getCorrectCount());
+        assertEquals(4, json.load("src\\test\\resources\\test.json").getCorrectCount());
     }
 }
