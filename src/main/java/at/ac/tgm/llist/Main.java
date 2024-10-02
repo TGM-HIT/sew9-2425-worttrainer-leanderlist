f513cb396d95d9e1de6480e045f1804bfa555d9e
Leander List <llist@student.tgm.ac.at>
2024-10-02 13:45:31 +0200

    Add comments to Main

--- src/main/java/at/ac/tgm/llist/Main.java
+++ src/main/java/at/ac/tgm/llist/Main.java
@@ -3,7 +3,16 @@ package at.ac.tgm.llist;
 import at.ac.tgm.llist.Controller.WorttrainerController;
 import at.ac.tgm.llist.Model.Persistence.JsonPersistence;
 
+/**
+ * @author Leander-List
+ * @version 02-10-2024
+ * Main class to start the application
+ */
 public class Main {
+    /**
+     * Main method to start the application
+     * @param args the arguments
+     */
     public static void main(String[] args) {
         new WorttrainerController(new JsonPersistence());
     }
